8f547ca9417f72fea3cd205f32d90433cf2513f4
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-10-04 21:57:23 -0700

    Added Output Typings

--- mod.ts
+++ mod.ts
@@ -1,7 +1,9 @@
 import dirname from "./mod/dirname.ts";
 import filename from "./mod/filename.ts";
 
-export default function generate(meta: ImportMeta) {
+export default function generate(
+  meta: ImportMeta,
+): { dirname: string; filename: string } {
   return {
     dirname: dirname(meta),
     filename: filename(meta),
