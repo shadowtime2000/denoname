66ff7e5d047f24e6c5178694e414e3865235481b
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-10-06 19:43:01 -0700

    Increase Coverage

--- mod/path_test.ts
+++ mod/path_test.ts
@@ -1,4 +1,4 @@
-import { assert } from "https://deno.land/std/testing/asserts.ts";
+import { assert, assertThrows } from "https://deno.land/std/testing/asserts.ts";
 import path from "./path.ts";
 
 Deno.test({
@@ -7,5 +7,6 @@ Deno.test({
     const __path = path(import.meta);
     assert(!__path.startsWith("file:///"));
     assert(/[\\|//]/.test(__path));
+    assertThrows(() => path({ url: "https://foo.bar", main: false }), Error, "You should not be using this in a hosted a script!");
   },
 });
