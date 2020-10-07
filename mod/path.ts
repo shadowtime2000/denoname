0dd04e6199233f0e9f5d4487d2a5513d5812917d
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-10-06 19:35:33 -0700

    Changes

--- mod/path.ts
+++ mod/path.ts
@@ -1,6 +1,6 @@
 export default (meta: ImportMeta): string => {
     if (meta.url.startsWith("file:///")) return meta.url.slice(8, meta.url.length);
-    else if (meta.url.startsWith("http://")) return meta.url.slice(7, meta.url.length);
-    else if (meta.url.startsWith("https://")) return meta.url.slice(8, meta.url.length);
-    else if (true) return meta.url;
+    else {
+        throw new Error("You should not be using this in a hosted a script!");
+    }
 };
