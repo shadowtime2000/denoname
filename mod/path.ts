b50df1c46afa5bce3ecb9fd9640b8976968d3cba
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-10-06 19:20:47 -0700

    Created if/else statement chain

--- mod/path.ts
+++ mod/path.ts
@@ -1,1 +1,6 @@
-export default (meta: ImportMeta): string => meta.url.slice(8, meta.url.length);
+export default (meta: ImportMeta): string => {
+    if (meta.url.startsWith("file:///")) return meta.url.slice(8, meta.url.length);
+    else if (meta.url.startsWith("http://")) return meta.url.slice(7, meta.url.length);
+    else if (meta.url.startsWith("https://")) return meta.url.slice(8, meta.url.length);
+    else if (true) return meta.url;
+};
