c3e6e6fb643b32a836e1a02602f4b1d1c64117f7
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-10-06 20:08:20 -0700

    Moved Output Type to mod/output.ts

--- mod.ts
+++ mod.ts
@@ -1,10 +1,11 @@
 import dirname from "./mod/dirname.ts";
 import path from "./mod/path.ts";
 import filename from "./mod/filename.ts";
+import { Output } from "./mod/output.ts";
 
 export default function generate(
   meta: ImportMeta,
-): { dirname: string; path: string; filename: string } {
+): Output {
   return {
     dirname: dirname(meta),
     path: path(meta),
---
+++ mod/output.ts
@@ -0,0 +1,7 @@
+type Output = {
+    dirname: string;
+    path: string;
+    filename: string;
+}
+
+export type { Output };
